4580ea1316e0b7e1e175f923bef2926551e92761
Michael Bowen <mikee.bowen@gmail.com>
2015-05-13 11:02:19 -0700

    create working put request and tests for delete and put

--- routes/teams_routes.js
+++ routes/teams_routes.js
@@ -38,68 +38,31 @@ module.exports = function (router) {
     })
   });
 
-  // router.put('/teams/updateteam/:id', function (req, res) {
-  //   var updatedTeam = req.body;
-  //   Team.find({_id: req.params.id}, function (err, data) {
-  //     if (err) {
-  //       console.log(err);
-  //       return res.status(406).json({msg: 'team not found'});
-  //     }
-  //   });
-
-  //   delete updatedTeam._id;
-
-  //   Team.findOneAndUpdate({'_id': req.params.id}, updatedTeam, function (err, data) {
-  //     if (err && req.body.wins + req.body.losses < 162) {
-  //       console.log(err);
-  //       return res.status(500).json({msg: 'internal server error'});
-  //     }
-  //     if (err) {
-  //       console.log(err);
-  //       return res.status(406).json({msg: 'team not found'})
-  //     }
-
-  //     res.json({msg: updatedTeam.name + ' has been updated'});
-  //   })
-  // })
-
-  // router.patch('/teams/newteam/:id', function (req, res) {
-  //   var patchTeam = new Team(req.body);
-
-  //   Team.find({_id: req.params.id}, function (err, data) {
-  //     if (err) {
-  //       delete patchTeam._id;
-  //     }
-  //   });
-
-  //   patchTeam.save(function (err, data) {
-  //     if (err && req.body.wins + req.body.losses < 162) {
-  //       console.log(err);
-  //       return res.status(500).json({msg: 'internal server error'});
-  //     }
-  //     if (err && req.body.wins + req.body.losses > 162) {
-  //       console.log(err);
-  //       return res.status(406).json({msg: 'team cannot have more than 162 games'});
-  //     }
-
-  //     res.json(data);
-  //   })
-  // })
-
-  // router.delete('/teams/delete/:id', function (req, res) {
-  //   Team.remove({'_id': req.params.id}, function (err, data) {
-  //     if (err) {
-  //       console.log(err);
-  //       return res.status(500).json({msg: 'internal server error'});
-  //     }
-  //     res.json({msg: 'team deleted'});
-  //   });
-  // });
+  router.put('/teams/updateteam/:id', function (req, res) {
+    sql.sync()
+    .then(function () {
+      Team.find(req.params.id)
+      .then(function (data) {
+        data.updateAttributes({
+          name: req.body.name,
+          wins: req.body.wins,
+          losses: req.body.losses
+        })
+        .then(function () {
+          return res.json({msg: req.body.name + ' has been updated'});
+        })
+        .error(function (err) {
+          console.log(err);
+          return res.json({msg: 'internal server error'});
+        })
+      })
+    })
+  });
 
   router.delete('/teams/delete/:id', function (req, res) {
     sql.sync()
     .then(function () {
-      Team.find({'id': req.params.id})
+      Team.find(req.params.id)
       .then(function (data) {
         data.destroy()
         res.json({msg: 'team deleted'});
--- test/server_test.js
+++ test/server_test.js
@@ -32,99 +32,41 @@ describe('Test that server can load a page', function () {
 });
 
 describe('Test Requests', function () {
+  var randomId = Math.ceil(Math.random() * 300);
 
   it('Should respond to POST request by creating a new team', function (done) {
     chai.request('localhost:3000')
     .post('/api/teams/addteam')
-    .send({id: -1, name: 'Mariners', wins: 12, losses: 17})
+    .send({id: randomId, name: 'Test Team', wins: 12, losses: 17})
     .end(function (err, res) {
       expect(err).to.eql(null);
       expect(typeof res.body).to.eql('object');
-      expect(res.body.name).to.eql('Mariners');
+      expect(res.body.name).to.eql('Test Team');
       expect(res.body.wins).to.eql(12);
       expect(res.body.losses).to.eql(17);
       done();
     })
   })
 
-  // beforeEach(function (done) {
-  //   var testTeam = new Team({name: 'Test Team', wins: 1, losses: 1});
-  //   testTeam.save(function (err, data) {
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     this.testTeam = data;
-  //     done();
-  //   }.bind(this));
-  // });
+  it('Should respond to PUT request by updating a team', function (done) {
+    chai.request('localhost:3000')
+    .put('/api/teams/updateteam/' + randomId)
+    .send({name: 'Red Sox', wins: 1, losses: 100})
+    .end(function (err, res) {
+      expect(err).to.eql(null);
+      expect(res.body.msg).to.eql('Red Sox has been updated');
+      done();
+    })
+  })
 
-  // beforeEach(function (done) {
-  //   sql.sync()
-  //   .then(function () {
-  //     Team.send({id: 'test id', name: 'Test Team', wins: 1, losses: 1})
-  //     .then(function (data) {
-  //       res.json(data);
-  //     })
-  //     .error(function (err) {
-  //       console.log(err);
-  //       res.status(500).json({msg: 'internal server error'});
-  //     })
-  //     done();
-  //   })
-  // })
-
-  // it('should make a test team beforeEach test', function () {
-  //   expect(this.testTeam.name).to.eql('Test Team');
-  //   expect(this.testTeam.wins).to.eql(1);
-  //   expect(this.testTeam.losses).to.eql(1);
-  // })
-
-  // // test get request
-  // it('Should respond to GET request with an array of teams', function (done) {
-  //   chai.request('localhost:3000')
-  //   .get('/api/teams')
-  //   .end(function (err, res) {
-  //     expect(err).to.eql(null);
-  //     expect(Array.isArray(res.body)).to.eql(true);
-  //     done();
-  //   })
-  // })
-
-  // it('Should respond to POST request by creating a new team', function (done) {
-  //   chai.request('localhost:3000')
-  //   .post('/api/teams/addteam')
-  //   .send({name: 'Mariners', wins: 12, losses: 17})
-  //   .end(function (err, res) {
-  //     expect(err).to.eql(null);
-  //     expect(typeof res.body).to.eql('object');
-  //     expect(res.body.name).to.eql('Mariners');
-  //     expect(res.body.wins).to.eql(12);
-  //     expect(res.body.losses).to.eql(17);
-  //     expect(res.body).to.have.property('_id');
-  //     done();
-  //   })
-  // })
-
-  // it('Should respond to a PUT request by updating an existing team', function (done) {
-  //   chai.request('localhost:3000')
-  //   .put('/api/teams/updateteam/' + this.testTeam._id)
-  //   .send({name: 'Mariners', wins: 12, losses: 17})
-  //   .end(function (err, res) {
-  //       expect(err).to.eql(null);
-  //       expect(res.body.msg).to.eql('Mariners has been updated');
-  //       done();
-  //     });
-  // });
-
-  // it('Should delete a team with a DELETE request', function (done) {
-  //   chai.request('localhost:3000')
-  //   .del('/api/teams/delete/' + this.testTeam._id)
-  //   .end(function (err, res) {
-  //     expect(err).to.eql(null);
-  //     expect(res.body.msg).to.eql('team deleted');
-  //     done();
-  //   });
-  // });
-
+  it('Should delete a team with a DELETE request', function (done) {
+    chai.request('localhost:3000')
+    .del('/api/teams/delete/' + randomId)
+    .end(function (err, res) {
+      expect(err).to.eql(null);
+      expect(res.body.msg).to.eql('team deleted');
+      done();
+    })
+  })
 
 })
